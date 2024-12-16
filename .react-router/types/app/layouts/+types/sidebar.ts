cbf420b5105e2710a953df5eec289cd1a70328ef
lusan <lusan.das@phonepe.com>
2024-12-16 08:43:25 +0530

    feat: added layout routes

--- .react-router/types/app/layouts/+types/sidebar.ts
+++ .react-router/types/app/layouts/+types/sidebar.ts
@@ -1,17 +1,16 @@
 // React Router generated types for route:
-// routes/home.tsx
+// layouts/sidebar.tsx
 
 import type * as T from "react-router/route-module"
 
 import type { Info as Parent0 } from "../../+types/root.js"
-import type { Info as Parent1 } from "../../layouts/+types/sidebar.js"
 
-type Module = typeof import("../home.js")
+type Module = typeof import("../sidebar.js")
 
 export type Info = {
-  parents: [Parent0, Parent1],
-  id: "routes/home"
-  file: "routes/home.tsx"
+  parents: [Parent0],
+  id: "layouts/sidebar"
+  file: "layouts/sidebar.tsx"
   path: "undefined"
   params: {} & { [key: string]: string | undefined }
   module: Module
--- .react-router/types/app/routes/+types/contact.ts
+++ .react-router/types/app/routes/+types/contact.ts
@@ -4,11 +4,12 @@
 import type * as T from "react-router/route-module"
 
 import type { Info as Parent0 } from "../../+types/root.js"
+import type { Info as Parent1 } from "../../layouts/+types/sidebar.js"
 
 type Module = typeof import("../contact.js")
 
 export type Info = {
-  parents: [Parent0],
+  parents: [Parent0, Parent1],
   id: "routes/contact"
   file: "routes/contact.tsx"
   path: "contacts/:contactId"
